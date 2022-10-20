840dc5acfb7b5976846f8becb4fd5951fbba3a9c
Itiel Edri <etledri1998@gmail.com>
2022-10-21 00:04:48 +0200

    trying to add gitignore

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 import os
 
-
+os.environ["SECRET_KEY"] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
